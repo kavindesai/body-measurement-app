b31418c98fb00ba016378513bb3087f32e25bc07
kavindesai <kavindesai261@gmail.com>
2018-10-19 06:31:20 -0400

    Add component html rendered

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EndpointsService } from '../endpoints.service';
+import {ActivatedRoute} from '@angular/router';
+import { Router } from '@angular/router';
+import { NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  email = '';
+  attribute = '';
+  year  = '';
+  month = '';
+  date  = '';
+  new_value;
+  constructor(private _endpoints: EndpointsService, private _route: ActivatedRoute, private _router: Router) {
+    this._route.params.subscribe( params => {
+      this.email = params.email;
+      this.attribute = params.attribute;
+    } );
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(this.year);
+    console.log(this.month);
+    console.log(this.date);
+    console.log(this.new_value);
   }
 
 }
